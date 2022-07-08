2d6c17b178ba000754862c0ec05e9c696ee7e972
German Guerrero <40763536+germandgm@users.noreply.github.com>
2022-07-07 23:43:14 -0300

    Add files via upload

---
+++ Desafio 2/desafio2.js
@@ -0,0 +1,16 @@
+//fecha manera linda: new Date(Date.now()).toLocaleString()
+const Contenedor = require('./clase.js')
+
+
+let miClase = new Contenedor();
+
+//miClase.save({"title":"halo","price":350,"thumbnail":"test-thumb"});
+miClase.getById(3);
+//miClase.getAll();
+//miClase.deleteById(2);
+//miClase.deleteAll();
+
+
+
+
+
